77d89ea52e9b815d593831727112e62ba79c2993
albasychris <albasychris@gmail.com>
2020-08-28 11:34:23 +1200

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Home from './components/Home/Home'
+import About from './components/About/About'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
+      <Navbar />
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/about' component={About}/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import './navbar.css'
+import { Link } from 'react-router-dom'
 class Navbar extends Component {
   render() {
     return (
-    <div className = "navbar">
-      <div className="navbar-left">
-      <h2 className="navbar-brand">Tourism NZ</h2>
-      <a className = "navbar-link" href="">Home</a>
-      <a className = "navbar-link" href="">About</a>
+      <div className="navbar">
+        <div className="navbar-left">
+          <h2 className="navbar-brand">Tourism NZ</h2>
+          <Link className="navbar-link" to="/">Home</Link>
+          <Link className="navbar-link" to="/about">About</Link>
+        </div>
+        <div className="navbar-right">
+          <button>Sign In</button>
+          <button>Sign Up</button>
+        </div>
+
       </div>
-      <div className ="navbar-right">
-      <button>Sign In</button>
-      <button>Sign Up</button>
-      </div>
-      
-    </div>
-  )}
+    )
+  }
 }
 
 export default Navbar
