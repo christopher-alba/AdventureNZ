822b935f6971d933290d60ffa69f07910c619309
albasychris <albasychris@gmail.com>
2020-08-29 08:30:43 +1200

    include api key inside .env file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import './home.css'
 import { Dropdown, Button } from 'semantic-ui-react'
 
-
 const Home = () => {
   let [forcastTenDays, setForcastTenDays] = useState()
   let [selectedItem, setSelectedItem] = useState()
@@ -29,12 +28,15 @@ const Home = () => {
   useEffect(() => {
 
   })
+  const getActivities = () => {
+    
+  }
   const fetchWeather = () => {
     fetch(`https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=${selectedItem}&lat=0&lon=0&cnt=10&units=metric`, {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": "34092fd749mshf784e4a6a066a34p158e4cjsna61883e3c77d"
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY
       }
     })
       .then(response => {
@@ -48,16 +50,91 @@ const Home = () => {
         setSelectedItemHeader(selectedItem)
 
       })
+      .then(() => {
+        document.getElementsByClassName("cityName")[0].scrollIntoView()
+      })
       .catch(err => {
         console.log(err);
+        console.log(process.env.API_KEY);
       });
   }
 
+  const getDate = (date) => {
+    let day = date.getDate()
+    let month = date.getMonth()
+    let weekday = date.getUTCDay()
+
+    switch (weekday) {
+      case 0:
+        weekday = "Sunday"
+        break;
+      case 1:
+        weekday = "Monday"
+        break;
+      case 2:
+        weekday = "Tuesday"
+        break;
+      case 3:
+        weekday = "Wednesday"
+        break;
+      case 4:
+        weekday = "Thursday"
+        break;
+      case 5:
+        weekday = "Friday"
+        break;
+      case 6:
+        weekday = "Saturday"
+        break;
+      default:
+        break;
+    }
+
+    switch (month) {
+      case 0:
+        month = "January"
+        break;
+      case 1:
+        month = "February"
+        break;
+      case 2:
+        month = "March"
+        break;
+      case 3:
+        month = "April"
+        break;
+      case 4:
+        month = "May"
+        break;
+      case 5:
+        month = "June"
+        break;
+      case 6:
+        month = "July"
+        break;
+      case 7:
+        month = "August"
+        break;
+      case 8:
+        month = "September"
+        break;
+      case 9:
+        month = "October"
+        break;
+      case 10:
+        month = "November"
+        break;
+      case 11:
+        month = "December"
+        break;
+    }
+    return `${weekday} ${day} ${month}`
+  }
 
   return (
     <>
       <div className="home-landing-container">
-        <img className="home-landing" src="https://silvertrips.in/wp-content/uploads/2019/06/new-zealand.jpg" alt="" />
+
       </div>
       <div className="home-landing-text">
         <h1>Welcome to New Zealand</h1>
@@ -70,40 +147,45 @@ const Home = () => {
           </div>
 
         </div>
+        <div className="home-selection">
+          <h2>Get Weather and Find Activities</h2>
+          <Dropdown
+            placeholder='Select City'
+            fluid
+            selection
+            options={cityOptions}
+            onChange={async (evt) => {
+              console.log(evt.currentTarget.innerText)
+              await setSelectedItem(evt.currentTarget.innerText)
+            }}
+          />
+          <Button onClick={() => fetchWeather()}>Get Weather for Next 10 Days</Button>
+
+
+        </div>
       </div>
-      <div class="home-selection">
-        <Dropdown
-          placeholder='Select City'
-          fluid
-          selection
-          options={cityOptions}
-          onChange={async (evt) => {
-            console.log(evt.currentTarget.innerText)
-            await setSelectedItem(evt.currentTarget.innerText)
-            fetchWeather()
-          }}
-        />
-        <Button onClick={() => fetchWeather()}>Get Weather for Next 10 Days</Button>
-        <h3 style={{paddingTop:"10%"}}>{selectedItemHeader}</h3>
+
+      <div className="weather-main-container">
+        <h3 className="cityName" style={{ paddingTop: "10%", fontSize: "3rem", color: "white" }}>{selectedItemHeader}</h3>
         <div className="all-forcasts">
-          
+
           {forcastTenDays && forcastTenDays.map(forcast => {
             console.log(forcast);
             let date = new Date(forcast.dt * 1000)
+            date = getDate(date)
             return (
-              <div className ="single-forcast">
-                <p>Date: {date.toString()}</p>
-                <p>Temperature (Day): {forcast.feels_like.day}</p>
-                <p>Temperature (Night): {forcast.feels_like.night}</p>
-                <p>Temperature (Evening): {forcast.feels_like.eve}</p>
-                <p>Temperature (Morning): {forcast.feels_like.morn}</p>
-                <p>Weather: {forcast.weather[0].main}, {forcast.weather[0].description}</p>
+              <div className="single-forcast" onClick={() => getActivities()}>
+                <div className="date info">{date}</div>
+                <div className="day info"><strong>Day:</strong> {forcast.feels_like.day} °C </div>
+                <div className="night info"><strong>Night: </strong> {forcast.feels_like.night} °C</div>
+                <div className="evening info"><strong> Evening:</strong> {forcast.feels_like.eve} °C</div>
+                <div className="morning info"> <strong>Morning:</strong> {forcast.feels_like.morn} °C</div>
+                <div className="weather info"><strong>Weather: </strong>{forcast.weather[0].main}, {forcast.weather[0].description}</div>
               </div>
             )
           })}
         </div>
       </div>
-
     </>
   )
 }
