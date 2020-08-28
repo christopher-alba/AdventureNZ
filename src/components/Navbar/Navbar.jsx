98f54a2206d4437c5ece70e83589d6d9b824e722
albasychris <albasychris@gmail.com>
2020-08-28 16:53:02 +1200

    make navbar show and hide

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,26 @@ import './navbar.css'
 import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
 class Navbar extends Component {
+
+  state = {
+    navbarToggle: true
+  }
+
+  componentDidMount = () => {
+    var prevScrollpos = window.pageYOffset
+    window.onscroll = () => {
+      var currentScrollPos = window.pageYOffset
+      if (prevScrollpos > currentScrollPos) {
+        this.setState({ navbarToggle: true })
+      } else {
+        this.setState({ navbarToggle: false })
+      }
+      prevScrollpos = currentScrollPos
+    }
+  }
   render() {
     return (
-      <div className="navbar">
+      <div className={`navbar ${this.state.navbarToggle ? "show" : "hide"}`}>
         <div className="navbar-left">
           <div className="navbar-brand">Tourism NZ</div>
           <Link className="navbar-link" to="/">Home</Link>
@@ -16,7 +33,7 @@ class Navbar extends Component {
           <Button color='purple'>Sign Up</Button>
         </div>
 
-      </div>
+      </div >
     )
   }
 }
