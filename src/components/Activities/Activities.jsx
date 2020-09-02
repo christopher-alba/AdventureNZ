25cb28b72d0ce46d5feb4452cde478db5a39e5a4
albasychris <albasychris@gmail.com>
2020-09-02 22:24:58 +1200

    include different types of activities for all cities/locations

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -3,8 +3,14 @@ import React, { Component, useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import './activities.css'
 import { getDate } from '../helpers'
+import { Button } from 'semantic-ui-react'
 
 class activities extends Component {
+
+  state = {
+    activity: "tourism",
+  }
+
   componentDidMount = () => {
     document.documentElement.scrollTop = 0
   }
@@ -26,11 +32,24 @@ class activities extends Component {
         </div>
         <div className="activities-list">
           <h1>Activities when {this.props.dayInfo.weather[0].description}</h1>
+          { !this.props.dayInfo.weather[0].main.includes("Rain") && <Button onClick={() => {
+            this.setState({activity:"Parks"})
+          }}>Parks</Button>}
+          <Button onClick={() => {
+            this.setState({activity:"Cafes"})
+          }}>Cafes</Button>
+          <Button onClick={() => {
+            this.setState({activity:"Tourism"})
+          }}>General Tourism</Button>
+          <Button onClick={() => {
+            this.setState({activity:"Indoors"})
+          }}>Indoors</Button>
           <iframe
-            width="600"
-            height="450"
-            frameBorder="0" style={{border:"0"}}
-            src={`https://www.google.com/maps/embed/v1/search?key=${process.env.REACT_APP_API_KEY_GOOGLE}&q=Auckland+parks`} allowFullScreen>
+            width="100%"
+            height="500px"
+            frameBorder="0" style={{ border: "0" }}
+            src={`https://www.google.com/maps/embed/v1/search?key=${process.env.REACT_APP_API_KEY_GOOGLE}&q=${this.props.cityName}+city+${this.state.activity}&zoom=8
+            `} allowFullScreen>
           </iframe>
         </div>
       </>
