0449579541b26aa2ae6639d40514c41f0cf45c1b
albasychris <albasychris@gmail.com>
2020-09-22 13:39:42 +1200

    add a responsive navbar (incomplete)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
-import { Button } from 'semantic-ui-react'
+import { Button, Responsive, Segment  } from 'semantic-ui-react'
 class Navbar extends Component {
 
   state = {
@@ -22,18 +22,31 @@ class Navbar extends Component {
   }
   render() {
     return (
-      <div className={`navbar ${this.state.navbarToggle ? "show" : "hide"}`}>
-        <div className="navbar-left">
-          <div className="navbar-brand">Tourism NZ</div>
-          <Link className="navbar-link" to="/">Home</Link>
-          <Link className="navbar-link" to="/about">About</Link>
-        </div>
-        <div className="navbar-right">
-          <Button primary>Sign In</Button>
-          <Button color='purple'>Sign Up</Button>
-        </div>
+      <Segment.Group>
+        <Responsive as={Segment} minWidth={1000}>
+          <div className={`navbar ${this.state.navbarToggle ? "show" : "hide"}`}>
+            <div className="navbar-left">
+              <div className="navbar-brand">Tourism NZ</div>
+              <Link className="navbar-link" to="/">Home</Link>
+              <Link className="navbar-link" to="/about">About</Link>
+            </div>
+            <div className="navbar-right">
+              <Button primary>Sign In</Button>
+              <Button color='purple'>Sign Up</Button>
+            </div>
 
-      </div >
+          </div >
+        </Responsive>
+        <Responsive as={Segment} minWidth={0} maxWidth={1000}>
+          <div className={`navbar ${this.state.navbarToggle ? "show" : "hide"}`}>
+            <div className="navbar-left">
+              <div className="navbar-brand">Tourism NZ</div>
+              <Link className="navbar-link" to="/">Home</Link>
+              <Link className="navbar-link" to="/about">About</Link>
+            </div>
+          </div >
+        </Responsive>
+      </Segment.Group>
     )
   }
 }
